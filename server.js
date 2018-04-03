4eab96ed8c0667ea6b2aec0b1ed502627a9eb833
Thennavan Ramalingam <thennavanr@gmail.com>
2018-04-03 00:53:07 -0400

    getting dynamic path

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.get('/contact', function (req, res) {
    res.render('contact');
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT ||3000, function () {
   console.log('server starting...');
 })
